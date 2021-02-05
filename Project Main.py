deec3a80cb50a79a4b59c6c026c202dcf7cd27b7
RussellPaine <russell.d.paine@gmail.com>
2021-02-05 15:47:18 +0000

    Refactored your project code to make a bit more sense to me if i was doing it. Added a few TODO:

--- Project Main.py
+++ Project Main.py
@@ -1,204 +1,222 @@
+# importing required modules
 import numpy as np
 import matplotlib.pyplot as mplt
 import scipy.integrate as integrate
-#importing required modules
 
-#################### Constants ###########################
-M_pl = 2.43*10**(18)        #Planck mass in GeV
-M_pl_2 = M_pl**2            #Planck mass squared
-M_pl_3 = M_pl**3            #Planck mass cubed
-M_pl_4 = M_pl**4            #Planck mass to the power of 4
-Omega_m0 = 0.32             #Present matter density fraction
-Omega_de0 = 0.68            #Present dark energy density fraction
-rho_c = 3.7468*10**(-47)    #Critical energy density in GeV^4
-rho_m0 = Omega_m0 * rho_c   #Present matter energy density in GeV^4
-rho_de0 = Omega_de0 * rho_c #Present dark energy energy density in GeV^4
-n = 3                       #Dominant energy component (matter)
-a_0 = 1                     #Present scale factor
-##########################################################
+# Constants
+M_PL = 2.43*10**(18) # Planck mass in GeV
+M_PL_2 = M_PL**2 # Planck mass squared
+M_PL_3 = M_PL**3 # Planck mass cubed
+M_PL_4 = M_PL**4 # Planck mass to the power of 4
+OMEGA_M0 = 0.32 # Present matter density fraction
+OMEGA_DE0 = 0.68 # Present dark energy density fraction
+RHO_C = 3.7468*10**(-47) # Critical energy density in GeV^4
+RHO_M0 = OMEGA_M0 * RHO_C # Present matter energy density in GeV^4
+RHO_DE0 = OMEGA_DE0 * RHO_C # Present dark energy energy density in GeV^4
+N = 3 # Dominant energy component (matter)
 
-#################### Input Values ########################
-#lambdo = 5                 #Model parameter
-z_i = 20                    #Initial redshift
-z_c = 10                    #Redshift at crossover
-eta = 0.5                   #Model parameter after crossover
-##########################################################
-
-################# Derived parameters #####################
-a_c = 1/(z_c + 1) #Scale factor at crossover
-lambdo = ((n*(2*Omega_de0 + (a_0/a_c)**3 *Omega_m0))/(2*Omega_de0))**(1/2)
-#First step of calculating initial conditions; obtaining the model parameter lamda (lambdo) or the redshift at crossover, depending on choice of input
-phi_c = M_pl/lambdo * (4*np.log(M_pl) - np.log(rho_de0))
-#Second step; obtaining the scalar field value at crossover
-a_i = 1/(z_i +1) #Initial scale factor
-phi_i = M_pl/lambdo * (4*np.log(M_pl) + np.log(2*((lambdo**2)/n - 1)) - np.log((a_0/a_i)**3 * rho_m0))
-#Third step; obtaining initial scalar field value, must be less than crossover field value
-x_i = (((a_0/a_i)**3 * rho_m0)/(((lambdo**2)/n) -1))**(0.5)
-#Final step; obtain initial rate of change for the scalar field
-##########################################################
-
-################# Deriving initial time ##################
-rho_m_i = (a_0/a_i)**3 * rho_m0
-#Initial matter energy density
-rho_phi_i = 2 * M_pl_4 * np.exp(-lambdo*phi_i/M_pl)
-#Initial scalar energy density
-H_i = ((rho_m_i+rho_phi_i)/(3*M_pl_2))**(1/2)
-t_i = 2/(n*H_i)
-#Initial time derived with Hubble paramter
-##########################################################
-
-############### Initial values and timestep ##############
-phi = phi_i
-a = a_i
-x = x_i
-t = t_i
-z = z_i
-da = 0.0001 * a
-dt = (n*t/(2*a)) * da
-##########################################################
-
-################### Arrays for plotting ##################
-array_a = []
-array_z = []
-array_phi = []
-array_H = []
-array_barotropic_parameter = []
-array_physical_distance_integrand = [0]
-array_LCDM_integrand = [0]
-array_physical_distance = []
-array_LCDM_physical_distance = []
-array_luminosity_distance = []
-array_LCDM_luminosity_distance = []
-array_Omega = []
-##########################################################
-
-############### Functions for Integrals ##################
-#def integrand(a, H):
-    #return 1/(H * a**2)
-#def barotropic_integrand(a):
-    #return (3/a)*(1 + barotropic_parameter)
-#def physical_distance_integrand(a):
-    #return (1/a**2) * (Omega_m0 * a**(-3) )**(-1/2)
-##########################################################
+# Input Values
+#lambdo = 5 # Model parameter
+Z_I = 20 # Initial redshift
+Z_C = 10 # Redshift at crossover
+ETA = 0.5 # Model parameter after crossover
 
 
+def main():
+    """Program to ...."""
 
-while a < a_0:
-    ############### Pre-Transition loop ##################
-    if phi < phi_c:
-        scalar_potential = M_pl_4 * np.exp(-lambdo*phi/M_pl)
-        energy_density_phi = x**2 /2 + scalar_potential
-        pressure_phi = x**2 /2 - scalar_potential
-        energy_density_m = (a_0/a)**3 * rho_m0
-        barotropic_parameter = pressure_phi/energy_density_phi
-        H = ((energy_density_m+energy_density_phi)/(3*M_pl_2))**(1/2)
-        LCDM_H = ((rho_m0 * a**(-3) + rho_de0)/(3*M_pl_2))**(1/2)
-        #calculating values from inputs, these will be used to update the infintesimals
+    a_0 = 1 # Present scale factor
+    lambdo, phi_c, a, phi, x = derive_parameters(a_0) # Derived parameters
+    t = derive_initial_time(a, lambdo, phi, a_0) # Deriving initial time
 
-        dphi = x*dt
-        dx = (-6*x/(n*t) + lambdo*M_pl_3*np.exp(-lambdo*phi/M_pl)) * dt
-        dt = (n*t/(2*a)) * da
-        #solving scalar field equation pre-transition
+    # Initial values and timestep
+    da = 0.0001 * a
+    dt = (N*t/(2*a)) * da
 
-        array_a.append(a)
-        array_phi.append(phi)
-        array_z.append((1/a) - 1)
-        array_barotropic_parameter.append(barotropic_parameter)
-        array_physical_distance_integrand.append(array_physical_distance_integrand[-1] + (da/(H * a**2)))
-        array_LCDM_integrand.append(array_LCDM_integrand[-1] + (da/(LCDM_H * a**2)))
-        array_Omega.append(energy_density_phi/(energy_density_phi+energy_density_m))
-        #placing values into arrays so they can be plotted later
-        x = x + dx
-        phi = phi + dphi
-        a = a + da
-        t = t + dt
-        #progressing timestep and updating values
-        
-    ######################################################
+    # Structure to store values for plottting later
+    values = PlottingArrays()
 
-    ############### Post-transition loop #################
-    else:
-        scalar_potential = M_pl_4 * np.exp(-lambdo*phi_c/M_pl) * np.exp(lambdo*eta*(phi - phi_c)/M_pl)
-        energy_density_phi = x**2 /2 + scalar_potential
-        pressure_phi = x**2 /2 - scalar_potential
-        energy_density_m = (a_0/a)**3 * rho_m0
-        barotropic_parameter = pressure_phi/energy_density_phi
-        H = ((energy_density_m+energy_density_phi)/(3*M_pl_2))**(1/2)
-        LCDM_H = ((rho_m0 * a**(-3) + rho_de0)/(3*M_pl_2))**(1/2)
-        #calculating values from inputs, these will be used to update the infintesimals
+    # TODO: Commnet on why you are doing this
+    while a < a_0:
+        if phi < phi_c:
+            values, dt, x, phi, a, t = pre_transition(values, lambdo, phi, x, a_0, a, dt, t, da)
+        else:
+            values, dt, x, phi, a, t = post_transition(values, lambdo, phi_c, phi, x, a_0, a, dt, da, t)
 
-        dphi = x*dt
-        dx = (-3*H*x - (eta*lambdo/M_pl)*scalar_potential) * dt
-        dt = (1/(a*H))*da
-        #solving scalar field equation post-transition
+    values.calculate_physical_Luminosity_distance() # TODO: Comment on why this needs to be done after loop
+    plotGraphs(values) # Plot graphs from calculated values
 
-        array_a.append(a)
-        array_phi.append(phi)
-        array_z.append((1/a) - 1)
-        array_barotropic_parameter.append(barotropic_parameter)
-        array_physical_distance_integrand.append(array_physical_distance_integrand[-1] + (da/(H * a**2)))
-        array_LCDM_integrand.append(array_LCDM_integrand[-1] + (da/(LCDM_H * a**2)))
-        array_Omega.append(energy_density_phi/(energy_density_phi+energy_density_m))
-        #placing values into arrays so they can be plotted later
+def derive_parameters(a_0):
+    """Derives initial parameters from global variables"""
 
-        x = x + dx
-        phi = phi + dphi
-        a = a + da
-        t = t + dt
-        #progressing timestep and updating values
-    ######################################################
-    #print(LCDM_H, array_LCDM_integrand[-1])
+    a_c = 1/(Z_C + 1) #Scale factor at crossover
+
+    # First step of calculating initial conditions; obtaining the model parameter lamda (lambdo) or the redshift at crossover, depending on choice of input
+    lambdo = ((N*(2*OMEGA_DE0 + (a_0/a_c)**3 *OMEGA_M0))/(2*OMEGA_DE0))**(1/2)
+
+    # Second step; obtaining the scalar field value at crossover
+    phi_c = M_PL/lambdo * (4*np.log(M_PL) - np.log(RHO_DE0))
+
+    # Third step; obtaining initial scalar field value, must be less than crossover field value
+    a_i = 1/(Z_I +1) #Initial scale factor
+    phi_i = M_PL/lambdo * (4*np.log(M_PL) + np.log(2*((lambdo**2)/N - 1)) - np.log((a_0/a_i)**3 * RHO_M0))
+
+    # Final step; obtain initial rate of change for the scalar field
+    x_i = (((a_0/a_i)**3 * RHO_M0)/(((lambdo**2)/N) -1))**(0.5)
+    
+    return lambdo, phi_c, a_i, phi_i, x_i
 
 
-############ Physical and Luminosity Distance ############
-d_z_i = array_physical_distance_integrand[-1]
-array_physical_distance_integrand.pop()
-for val in array_physical_distance_integrand:
-    array_physical_distance.append(d_z_i - val)
+def derive_initial_time(a_i, lambdo, phi_i, a_0):
+    """Derives initial time parameters from global variables and passed in values"""
 
-for val_phys, val_a in zip(array_physical_distance,array_a):
-    array_luminosity_distance.append(1/val_a * val_phys)
-##########################################################
+    rho_m_i = (a_0/a_i)**3 * RHO_M0 # Initial matter energy density
+    rho_phi_i = 2 * M_PL_4 * np.exp(-lambdo*phi_i/M_PL) # Initial scalar energy density
 
-############ Physical and Luminosity Distance ############
-LCDM_d_z_i = array_LCDM_integrand[-1]
-array_LCDM_integrand.pop()
-for val in array_LCDM_integrand:
-    array_LCDM_physical_distance.append(LCDM_d_z_i - val)
+    # Initial time derived with Hubble paramter
+    H_i = ((rho_m_i+rho_phi_i)/(3*M_PL_2))**(1/2)
+    t_i = 2/(N*H_i)
+    
+    return t_i
 
-for val_phys, val_a in zip(array_LCDM_physical_distance,array_a):
-    array_LCDM_luminosity_distance.append(1/val_a * val_phys)
-##########################################################
+def pre_transition(values, lambdo, phi, x, a_0, a, dt, t, da):
+    """Calculate .... pre-transition"""
+    # TODO: What is being calculated here?
+
+    # Calculating values from inputs, these will be used to update the infintesimals
+    scalar_potential = M_PL_4 * np.exp(-lambdo*phi/M_PL)
+    energy_density_phi = x**2 /2 + scalar_potential
+    pressure_phi = x**2 /2 - scalar_potential
+    energy_density_m = (a_0/a)**3 * RHO_M0
+    barotropic_parameter = pressure_phi/energy_density_phi
+    H = ((energy_density_m+energy_density_phi)/(3*M_PL_2))**(1/2)
+    lcdm_H = ((RHO_M0 * a**(-3) + RHO_DE0)/(3*M_PL_2))**(1/2)
+    
+    # Solving scalar field equation pre-transition
+    dphi = x*dt
+    dx = (-6*x/(N*t) + lambdo*M_PL_3*np.exp(-lambdo*phi/M_PL)) * dt
+    dt = (N*t/(2*a)) * da
+    
+    # Placing values into arrays so they can be plotted later
+    values.a.append(a)
+    values.phi.append(phi)
+    values.z.append((1/a) - 1)
+    values.barotropic_parameter.append(barotropic_parameter)
+    values.physical_distance_integrand.append(values.physical_distance_integrand[-1] + (da/(H * a**2)))
+    values.lcdm_integrand.append(values.lcdm_integrand[-1] + (da/(lcdm_H * a**2)))
+    values.Omega.append(energy_density_phi/(energy_density_phi+energy_density_m))
+    
+    # Progressing timestep and updating values
+    x = x + dx
+    phi = phi + dphi
+    a = a + da
+    t = t + dt
+    
+    return values, dt, x, phi, a, t
 
 
-mplt.plot(array_z, array_phi, label = 'STFQ', color = 'r')
-mplt.xlabel('Redshift, z')
-mplt.ylabel('Phi')
-mplt.legend()
-mplt.show()
+def post_transition(values, lambdo, phi_c, phi, x, a_0, a, dt, da, t):
+    """Calculate .... post-transition"""
+    # TODO: What is being calculated here?
 
-mplt.plot(array_z, array_barotropic_parameter, label = 'STFQ', color = 'r')
-mplt.xlabel('Redshift, z')
-mplt.ylabel('Barotrpoic Parameter, w')
-mplt.legend()
-mplt.show()
+    # Calculating values from inputs, these will be used to update the infintesimals
+    scalar_potential = M_PL_4 * np.exp(-lambdo*phi_c/M_PL) * np.exp(lambdo*ETA*(phi - phi_c)/M_PL)
+    energy_density_phi = x**2 /2 + scalar_potential
+    pressure_phi = x**2 /2 - scalar_potential
+    energy_density_m = (a_0/a)**3 * RHO_M0
+    barotropic_parameter = pressure_phi/energy_density_phi
+    H = ((energy_density_m+energy_density_phi)/(3*M_PL_2))**(1/2)
+    lcdm_H = ((RHO_M0 * a**(-3) + RHO_DE0)/(3*M_PL_2))**(1/2)
+    
+    # Solving scalar field equation post-transition
+    dphi = x*dt
+    dx = (-3*H*x - (ETA*lambdo/M_PL)*scalar_potential) * dt
+    dt = (1/(a*H))*da
+    
+    # Placing values into arrays so they can be plotted later
+    values.a.append(a)
+    values.phi.append(phi)
+    values.z.append((1/a) - 1)
+    values.barotropic_parameter.append(barotropic_parameter)
+    values.physical_distance_integrand.append(values.physical_distance_integrand[-1] + (da/(H * a**2)))
+    values.lcdm_integrand.append(values.lcdm_integrand[-1] + (da/(lcdm_H * a**2)))
+    values.Omega.append(energy_density_phi/(energy_density_phi+energy_density_m))
+    
+    # Progressing timestep and updating values
+    x = x + dx
+    phi = phi + dphi
+    a = a + da
+    t = t + dt
+    
+    return values, dt, x, phi, a, t
 
-mplt.plot(array_z, array_physical_distance, label = 'STFQ', color = 'r')
-mplt.plot(array_z, array_LCDM_physical_distance, label = 'LCDM', color = 'c')
-mplt.xlabel('Redshift, z')
-mplt.ylabel('Physical Distance, d(z)')
-mplt.legend()
-mplt.show()
 
-mplt.plot(array_z, array_luminosity_distance, label = 'STFQ', color = 'r')
-mplt.plot(array_z, array_LCDM_luminosity_distance, label = 'LCDM', color = 'c')
-mplt.xlabel('Redshift, z')
-mplt.ylabel('Luminosity Distance, d_L(z)')
-mplt.legend()
-mplt.show()
+def plotGraphs(values):
+    """Plots Graph from given values"""
 
-#mplt.plot(array_z, array_Omega)
-#mplt.xlabel('Redshift, z')
-#mplt.ylabel('Phi Energy Density Fraction, Omega')
-#mplt.legend
-#mplt.show()
+    mplt.plot(values.z, values.phi, label = 'STFQ', color = 'r')
+    mplt.xlabel('Redshift, z')
+    mplt.ylabel('Phi')
+    mplt.legend()
+    mplt.show()
+
+    mplt.plot(values.z, values.barotropic_parameter, label = 'STFQ', color = 'r')
+    mplt.xlabel('Redshift, z')
+    mplt.ylabel('Barotrpoic Parameter, w')
+    mplt.legend()
+    mplt.show()
+
+    mplt.plot(values.z, values.physical_distance, label = 'STFQ', color = 'r')
+    mplt.plot(values.z, values.lcdm_physical_distance, label = 'lcdm', color = 'c')
+    mplt.xlabel('Redshift, z')
+    mplt.ylabel('Physical Distance, d(z)')
+    mplt.legend()
+    mplt.show()
+
+    mplt.plot(values.z, values.luminosity_distance, label = 'STFQ', color = 'r')
+    mplt.plot(values.z, values.lcdm_luminosity_distance, label = 'lcdm', color = 'c')
+    mplt.xlabel('Redshift, z')
+    mplt.ylabel('Luminosity Distance, d_L(z)')
+    mplt.legend()
+    mplt.show()
+
+
+class PlottingArrays:
+    """Stores all array values needed to plot graphs"""
+    def __init__(self):
+        self.a = []
+        self.z = []
+        self.phi = []
+        self.barotropic_parameter = []
+        self.physical_distance_integrand = [0]
+        self.lcdm_integrand = [0]
+        self.physical_distance = []
+        self.lcdm_physical_distance = []
+        self.luminosity_distance = []
+        self.lcdm_luminosity_distance = []
+        self.Omega = []
+
+    def calculate_physical_Luminosity_distance(self):
+        """Calculate ...."""
+        # TODO: What is being calculated here?
+        # TODO: Added some comments here becuase this is a big ol chunk
+
+        d_z_i = self.physical_distance_integrand[-1]
+        self.physical_distance_integrand.pop()
+        for val in self.physical_distance_integrand:
+            self.physical_distance.append(d_z_i - val)
+
+        for val_phys, val_a in zip(self.physical_distance,self.a):
+            self.luminosity_distance.append(1/val_a * val_phys)
+
+        lcdm_d_z_i = self.lcdm_integrand[-1]
+        self.lcdm_integrand.pop()
+        for val in self.lcdm_integrand:
+            self.lcdm_physical_distance.append(lcdm_d_z_i - val)
+
+        for val_phys, val_a in zip(self.lcdm_physical_distance,self.a):
+            self.lcdm_luminosity_distance.append(1/val_a * val_phys)
+
+
+if __name__ == "__main__":
+    main()
